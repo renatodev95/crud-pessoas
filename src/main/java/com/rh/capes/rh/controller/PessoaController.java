bc57b483052e3c38a94878f060d094c13b9c1faa
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-01-27 11:51:10 -0300

    implementando a tela para incluir pessoa

--- src/main/java/com/rh/capes/rh/controller/PessoaController.java
+++ src/main/java/com/rh/capes/rh/controller/PessoaController.java
@@ -4,10 +4,9 @@ import com.rh.capes.rh.domain.Pessoa;
 import com.rh.capes.rh.repository.PessoaRepositorio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class PessoaController {
@@ -25,18 +24,9 @@ public class PessoaController {
     }
 
     @GetMapping("/rh/pessoas/nova")
-    public String getPessoaForm(Model model) {
-        model.addAttribute("listaPessoas", repositorio.findAll());
-        return "rh/pessoas/nova";
+    public String novaPessoa(@ModelAttribute("pessoa")Pessoa pessoa) {
+        return "rh/pessoas/form";
     }
 
-    @PostMapping("/rh/pessoas/nova")
-    public String postPessoaForm(Pessoa pessoa, BindingResult result, RedirectAttributes attributes) {
-        if(result.hasErrors()){
-            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
-            return "redirect:/rh/pessoas/nova";
-        }
-        repositorio.save(pessoa);
-        return "redirect:/rh/pessoas";
-    }
+
 }
